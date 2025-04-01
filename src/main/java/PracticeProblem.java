ca78d2dc6aaff44604d8cb07967858c15b594cef
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-04-01 17:57:09 +0000

    a

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,22 @@ public class PracticeProblem {
 
 	}
 
+	public static int sumOfDigits(int x) {
+		x = Math.abs(x);
+		if(x < 10) {
+			return x;
+		} 
+		
+		return sumOfDigits(x % 10) + sumOfDigits(x / 10);
+	}
 	
+	public static int factorial(int n) {
+		if(n == 0) {
+			return 1;
+		} if (n < 0) {
+			return 0;
+		}
+		return n * factorial(n - 1);
+	}
 
 }
